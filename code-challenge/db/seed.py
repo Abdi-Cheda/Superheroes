ab53bd1ff0c6917967aa346f0d7922fdb1648f41
Abdi-Cheda <abdicheda75@gmail.com>
2024-01-31 09:19:20 +0300

    commit

--- code-challenge/db/seed.py
+++ code-challenge/db/seed.py
@@ -1,35 +1,69 @@
-puts "🦸‍♀️ Seeding powers..."
-Power.create([
-  { name: "super strength", description: "gives the wielder super-human strengths" },
-  { name: "flight", description: "gives the wielder the ability to fly through the skies at supersonic speed" },
-  { name: "super human senses", description: "allows the wielder to use her senses at a super-human level" },
-  { name: "elasticity", description: "can stretch the human body to extreme lengths" }
-])
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+from random import randint, choice
 
-puts "🦸‍♀️ Seeding heroes..."
-Hero.create([
-  { name: "Kamala Khan", super_name: "Ms. Marvel" },
-  { name: "Doreen Green", super_name: "Squirrel Girl" },
-  { name: "Gwen Stacy", super_name: "Spider-Gwen" },
-  { name: "Janet Van Dyne", super_name: "The Wasp" },
-  { name: "Wanda Maximoff", super_name: "Scarlet Witch" },
-  { name: "Carol Danvers", super_name: "Captain Marvel" },
-  { name: "Jean Grey", super_name: "Dark Phoenix" },
-  { name: "Ororo Munroe", super_name: "Storm" },
-  { name: "Kitty Pryde", super_name: "Shadowcat" },
-  { name: "Elektra Natchios", super_name: "Elektra" }
-])
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superheroes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-puts "🦸‍♀️ Adding powers to heroes..."
+class Power(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(250))
 
-strengths = ["Strong", "Weak", "Average"]
-Hero.all.each do |hero|
-  rand(1..3).times do
-    # get a random power
-    power = Power.find(Power.pluck(:id).sample)
+class Hero(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    super_name = db.Column(db.String(100), nullable=False)
 
-    HeroPower.create!(hero_id: hero.id, power_id: power.id, strength: strengths.sample)
-  end
-end
+class HeroPower(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'), nullable=False)
+    power_id = db.Column(db.Integer, db.ForeignKey('power.id'), nullable=False)
+    strength = db.Column(db.String(50))
+# Create the tables
+db.create_all()
 
-puts "🦸‍♀️ Done seeding!"
+# Seeding the database
+def seed_data():
+    print("🦸‍♀️ Seeding powers...")
+    powers = [
+        {"name": "super strength", "description": "gives the wielder super-human strengths"},
+        {"name": "flight", "description": "gives the wielder the ability to fly through the skies at supersonic speed"},
+        {"name": "super human senses", "description": "allows the wielder to use her senses at a super-human level"},
+        {"name": "elasticity", "description": "can stretch the human body to extreme lengths"}
+    ]
+
+    for power in powers:
+        db.session.add(Power(**power))
+
+    print("🦸‍♀️ Seeding heroes...")
+    heroes = [
+        {"name": "Kamala Khan", "super_name": "Ms. Marvel"},
+        {"name": "Doreen Green", "super_name": "Squirrel Girl"},
+        # Add the rest of your heroes here...
+    ]
+
+    for hero in heroes:
+        db.session.add(Hero(**hero))
+
+    db.session.commit()
+
+    print("🦸‍♀️ Adding powers to heroes...")
+
+    strengths = ["Strong", "Weak", "Average"]
+    heroes = Hero.query.all()
+    for hero in heroes:
+        for _ in range(randint(1, 3)):  # Assuming you've imported randint from random
+            power = Power.query.order_by(func.random()).first()  # For SQLite
+            hero_power = HeroPower(hero_id=hero.id, power_id=power.id, strength=choice(strengths))  # Assuming you've imported choice from random
+            db.session.add(hero_power)
+
+    db.session.commit()
+
+    print("🦸‍♀️ Done seeding!")
+
+if __name__ == '__main__':
+    seed_data()
