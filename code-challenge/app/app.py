aa42cd0825a672bffcc472f433b5bb55af4147ce
Abdi-Cheda <abdicheda75@gmail.com>
2024-02-01 12:24:22 +0300

    commit

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -5,8 +5,10 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superheroes.db'
 db = SQLAlchemy(app)
 
-from models import Hero, Power, HeroPower
 from routes import hero_routes, power_routes
 
+app.register_blueprint(hero_routes)
+app.register_blueprint(power_routes)
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
