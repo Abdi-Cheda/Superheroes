9bd4dbd1e630bf7cedb8dce7d5dfa4ebe929ad53
Abdi-Cheda <abdicheda75@gmail.com>
2024-02-01 09:45:57 +0300

    commit

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from app.models import Hero, Power, HeroPower
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superheroes.db'
