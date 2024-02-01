a7333bf350888f7547e649cc1dd8a39259b0c9d2
Abdi-Cheda <abdicheda75@gmail.com>
2024-02-01 09:21:10 +0300

    commit

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -1,32 +1,5 @@
-# app.py
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from config import Config
-from flask import jsonify
+from app import app
+from app.models import Hero, Power, HeroPower  # Import models here to avoid circular imports
 
-db = SQLAlchemy()
-migrate = Migrate()
-
-
-app = Flask(__name__)
-app.config.from_object(Config)
-
-db.init_app(app)
-migrate.init_app(app, db)
-
-with app.app_context():
-        from models import Hero, Power, HeroPower  # Import models here to avoid circular imports
-
-def setup_routes(app):
-    @app.route('/heroes', methods=['GET'])
-    def get_heroes():
-        from models import Hero  # Import within the function to avoid circular imports
-        heroes = Hero.query.all()
-        return jsonify([hero.serialize() for hero in heroes])
-    # Define other routes similarly
-        # Import routes
-        from routes import setup_routes
-        setup_routes(app)
-
-    return app
+if __name__ == '__main__':
+    app.run(debug=True, port=5555)
