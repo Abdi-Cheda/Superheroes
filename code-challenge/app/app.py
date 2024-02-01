264d7903e4bd1f9280b23b262d32cfb5a12d165c
Abdi-Cheda <abdicheda75@gmail.com>
2024-02-01 12:48:18 +0300

    commit

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Allow all origins for simplicity; you can configure this according to your needs
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superheroes.db'
 db = SQLAlchemy(app)
 
