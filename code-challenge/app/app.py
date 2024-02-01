2d8ed2460929b18095eef49063bbb576dea04703
Abdi-Cheda <abdicheda75@gmail.com>
2024-02-01 09:23:11 +0300

    commit

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -1,5 +1,12 @@
 from app import app
 from app.models import Hero, Power, HeroPower  # Import models here to avoid circular imports
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5555)
+
+    from app import routes  # Import routes after creating app to avoid circular import
