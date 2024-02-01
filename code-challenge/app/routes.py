363b178b5d12311a813da450717797415efd82ae
Abdi-Cheda <abdicheda75@gmail.com>
2024-02-01 10:09:03 +0300

    commit

--- code-challenge/app/routes.py
+++ code-challenge/app/routes.py
@@ -1,7 +1,7 @@
 from flask import jsonify, request
-from app import app
-from app import db
+from app import app, db
 from models import Hero, Power, HeroPower
+
 # Your routes and view functions
 
 @app.route('/heroes', methods=['GET'])
