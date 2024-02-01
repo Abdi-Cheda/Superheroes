80d8b2d27cd1e203ccc1195b595a5d6ae8219e9d
Abdi-Cheda <abdicheda75@gmail.com>
2024-02-01 11:34:21 +0300

    commit

--- code-challenge/app/models.py
+++ code-challenge/app/models.py
@@ -1,28 +1,27 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import validates
+
 db = SQLAlchemy()
 
 class Hero(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     super_name = db.Column(db.String(100), nullable=False)
-    powers = db.relationship('HeroPower', back_populates='hero', lazy='dynamic')
+    hero_powers = db.relationship('HeroPower', back_populates='hero', lazy='dynamic')
 
     def serialize(self):
         return {
             'id': self.id,
             'name': self.name,
             'super_name': self.super_name,
-            'powers': [power.serialize() for power in self.powers]
+            'powers': [power.serialize() for power in self.hero_powers]
         }
 
 class Power(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(255), nullable=False)
-    heroes = db.relationship('HeroPower', back_populates='power', lazy='dynamic')
+    hero_powers = db.relationship('HeroPower', back_populates='power', lazy='dynamic')
 
     def serialize(self):
         return {
@@ -33,11 +32,11 @@ class Power(db.Model):
 
 class HeroPower(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    strength = db.Column(db.String(50), nullable=False)
+    strength = db.Column(db.String(50), nullable=False, server_default='Average')
     hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'), nullable=False)
     power_id = db.Column(db.Integer, db.ForeignKey('power.id'), nullable=False)
-    hero = db.relationship('Hero', back_populates='powers')
-    power = db.relationship('Power', back_populates='heroes')
+    hero = db.relationship('Hero', back_populates='hero_powers')
+    power = db.relationship('Power', back_populates='hero_powers')
 
     @validates('strength')
     def validate_strength(self, key, strength):
