c85f5dde7d6054c15a0e4deb5516fb0d86fdeb44
Abdi-Cheda <abdicheda75@gmail.com>
2024-02-29 22:04:20 +0300

    commit

--- code-challenge/app/app.py
+++ code-challenge/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from .models import db, Power, Hero, HeroPower
 
@@ -60,6 +60,40 @@ def get_hero_powers():
         'hero_id': hero_power.hero_id,
         'power_id': hero_power.power_id
     } for hero_power in hero_powers])
+
+@app.route('/hero_powers', methods=['POST'])
+def create_hero_power():
+    data = request.get_json()
+    hero_id = data.get('hero_id')
+    power_id = data.get('power_id')
+    strength = data.get('strength')
+
+    if hero_id is None or power_id is None or strength is None:
+        return make_response(jsonify({'errors': ['Missing required data']}), 400)
+
+    hero = Hero.query.get(hero_id)
+    power = Power.query.get(power_id)
+
+    if not hero or not power:
+        return make_response(jsonify({'errors': ['Hero or Power not found']}), 404)
+
+    new_hero_power = HeroPower(hero=hero, power=power, strength=strength)
+
+    try:
+        db.session.add(new_hero_power)
+        db.session.commit()
+        return jsonify({
+            'id': hero.id,
+            'name': hero.name,
+            'super_name': hero.super_name,
+            'powers': [{
+                'id': power.id,
+                'name': power.name,
+                'description': power.description
+            }]
+        }), 201
+    except Exception as e:
+        return make_response(jsonify({'errors': [str(e)]}), 400)
 
 if __name__ == '__main__':
     app.run(port=5000)
